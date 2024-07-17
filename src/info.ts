e50d4251e1da5de2eb0c8f64f5cb7d5c6a376e1d
Justin Chase <justin.chase@optum.com>
2024-07-17 13:31:46 +0000

    0.8.10

--- src/info.ts
+++ src/info.ts
@@ -2,4 +2,4 @@
  * The current version, kept in sync with VERSION
  * via the `inc` command. Output with the `--version` flag.
  */
-export const version = "0.8.9";
+export const version = "0.8.10";
